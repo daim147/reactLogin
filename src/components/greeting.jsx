758c35ca881fd9a6239f70d78225711221b1f7dd
DaimSyed <daimshah784@gmail.com>
2021-06-12 00:34:13 +0500

    Restore

--- src/components/greeting.jsx
+++ src/components/greeting.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export const Greeting = ({ hour }) => {
-  if (hour >= 20 && hour < 8) {
+  if (hour >= 20 || hour < 8 || hour >= 0) {
     return <h1>Sweet Dream</h1>;
   } else if (hour >= 8) {
     return <h1>Have a Good Day</h1>;
